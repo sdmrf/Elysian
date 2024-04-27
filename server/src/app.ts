5f926b6905bc9b320c3732f594e58869846ba417
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-04-27 15:34:58 +0530

    feat : remove eslint

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,13 @@
 import express from 'express';
 import cors from 'cors';
 
-// Imprting the routes
+// Importing the routes
+
+
 // Creating an instance of express
 const app = express();
+
+
 // Middlewares
 // Base URL
 // Routes

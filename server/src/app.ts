8299c82ca3453c43c0e9ceda54685e04c5967e98
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-04-27 15:56:52 +0530

    feat : Add Basic Setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,8 +10,18 @@ const app = express();
 
 
 // Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Base URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the server');
+});
+
 // Routes
+
+
 // exporting the app
 export { app }
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
-import { app } from './app.js';
-import dotenv from 'dotenv';
-import { PORT, MONGO_URI, JWT_SECRET} from "./constants/constants.js"
-dotenv.config();
+// Imports
+import { app } from "./app.js";
+import { PORT } from "./constants/constants.js";
+import { connectDB } from "./database/database.js";
 
 // Listen to the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const appListen = () =>
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+
+// Conneting to the database
+connectDB()
+  .then(() => {
+    appListen();
+  })
+  .catch((err) => {
+    console.log(`Database connection failed: ${err}`);
+  });

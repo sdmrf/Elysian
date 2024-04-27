559131ef93f73329d110aa8416275f3867573fd6
s1lent-dev <deshpande.pxresh@gmail.com>
2024-04-27 20:17:21 +0530

    feat : Add error middleware

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,12 @@
+// Imports 
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../utils/utility-class.js";
+
+export const errorMiddleware = (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+  err.message ||= "Internal Server Error";
+  err.statusCode ||= 500;
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};

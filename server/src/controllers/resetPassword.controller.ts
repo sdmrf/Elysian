7918d38c7f3359d8d4880fec118d12734dbba27c
s1lent-dev <deshpande.pxresh@gmail.com>
2024-05-01 14:28:20 +0530

    refactor : refactor the reset password controller and model

--- server/src/controllers/resetPassword.controller.ts
+++ server/src/controllers/resetPassword.controller.ts
@@ -1,65 +1,64 @@
-// Imports
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
-// Models
 import { User } from "../models/user.model.js";
 import { ResetToken } from "../models/resetToken.model.js";
-
-// Handlers
 import { ErrorHandler } from "../utils/errorHandler.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { responseHandler } from "../utils/responseHandler.js";
-
-// Utils
 import { sendPasswordResetLink } from "../utils/resetPasswordHandler.js";
-
-// Constants
 import { RESET_PASSWORD_LINK } from "../constants/constants.js";
 
 
-//* Send reset password email
-const sendResetPasswordEmail = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return next(new ErrorHandler("User not found", 404));
-    
-    const newResetTokenSchema = await ResetToken.findOne({ userId: user._id });
-    const newToken = await bcrypt.hash(user._id, 10);
-
-    if(!newResetTokenSchema) {
-        const resetToken = new ResetToken({
-            userId: user._id,
-            token: newToken
-        });
-        await resetToken.save();
-    }
-    const resetLink = `${RESET_PASSWORD_LINK}/${newToken}`;
-
-    await sendPasswordResetLink(email, resetLink);
-
-    return res.status(200).json(new responseHandler(200, "Password reset link sent successfully", {}));
-  })
+//? Helper function to generate reset token
+const generateResetToken = async (userId: string) => {
+  const token = await bcrypt.hash(userId, 10);
+  const resetToken = new ResetToken({ userId, token });
+  await resetToken.save();
+  return token;
+};
 
 
-  
-//* Reset password controller
-const resetPassword = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
-    const { password } = req.body;
-    const { id } = req.params;
+//! Contrroller Functions
 
-    const newResetTokenSchema = await ResetToken.findOne({ token : id });
-    if (!newResetTokenSchema) return next(new ErrorHandler("Invalid or expired reset token", 400));
+//* send reset password email with reset link controller
+const sendResetPasswordEmail = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    const ismatch = await bcrypt.compare(newResetTokenSchema.userId, id);
-    if (!ismatch) return next(new ErrorHandler("Invalid or expired reset token", 400));
+  await ResetToken.deleteMany({ userId: user._id });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.findByIdAndUpdate(id, { password: hashedPassword }, { new: true });
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  const resetToken = await generateResetToken(user._id);
+  const resetLink = `${RESET_PASSWORD_LINK}/${resetToken}`;
 
-    await ResetToken.deleteMany({ userId: user._id });
+  await sendPasswordResetLink(email, resetLink);
 
-    return res.status(200).json(new responseHandler(200, "Password reset successfully", {}));
+  return res
+    .status(200)
+    .json(new responseHandler(200, "Password reset link sent successfully", {}));
 });
 
-export { sendResetPasswordEmail, resetPassword }
+
+//* reset password controller
+const resetPassword = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  const { id } = req.params;
+
+  const resetTokenSchema = await ResetToken.findOne({ token: id });
+  if (!resetTokenSchema) return next(new ErrorHandler("Invalid or expired reset token", 400));
+
+  const isMatch = await bcrypt.compare(resetTokenSchema.userId, id);
+  if (!isMatch) return next(new ErrorHandler("Invalid or expired reset token", 400));
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.findByIdAndUpdate(resetTokenSchema.userId, { password: hashedPassword }, { new: true });
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  await ResetToken.deleteMany({ userId: user._id });
+
+  return res
+    .status(200)
+    .json(new responseHandler(200, "Password reset successfully", {}));
+});
+
+export { sendResetPasswordEmail, resetPassword };
--- server/src/models/resetToken.model.ts
+++ server/src/models/resetToken.model.ts
@@ -1,33 +1,31 @@
 import mongoose, { Document, Schema } from "mongoose"; // Importing mongoose for schema and model creation
 import { resetTokenSchemaValidation } from "../validation/resetToken.validation.js";
 
-
 //! Interface for OTP document
 interface IResetToken extends Document {
-    userId: string;
-    token: string;
-    createdAt: Date;
+  userId: string;
+  token: string;
+  createdAt: Date;
 }
 
 //! reset Token Schema
 const resetTokenSchema = new Schema(
   {
-    userId : {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+    userId: {
+      type: String,
+      required: true,
     },
     token: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        expires: 3600, // This will be valid for 1 hour
-        get: (createdAt: Date) => createdAt.getTime(),
-        set: (createdAt: number) => new Date(createdAt),
-    }
+      type: Date,
+      default: Date.now,
+      expires: 3600, // This will be valid for 1 hour
+      get: (createdAt: Date) => createdAt.getTime(),
+      set: (createdAt: number) => new Date(createdAt),
+    },
   },
   { timestamps: true }
 );
@@ -40,4 +38,7 @@ resetTokenSchema.pre<IResetToken>("save", async function (next) {
 });
 
 //* export OTP model
-export const ResetToken = mongoose.model<IResetToken>("ResetToken", resetTokenSchema);
+export const ResetToken = mongoose.model<IResetToken>(
+  "ResetToken",
+  resetTokenSchema
+);

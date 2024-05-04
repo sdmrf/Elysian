0e4509a5abbf2dc376e72625eb81ea3426e4ec5e
s1lent-dev <deshpande.pxresh@gmail.com>
2024-05-04 15:15:53 +0530

    feat : add order controllers and restore stock feature handler

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -8,14 +8,18 @@ import { Order } from "../models/order.model.js";
 import { ErrorHandler } from "../utils/errorHandler.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { responseHandler } from "../utils/responseHandler.js";
-import { InvalidateCache, reduceStock } from "../utils/featuresHandler.js";
+import {
+  InvalidateCache,
+  reduceStock,
+  restoreStock,
+} from "../utils/featuresHandler.js";
 import { redisClient } from "../config/redis.config.js";
 import { OrderItemRequest } from "../types/types.js";
 
 // Controller Functions
 
 //* Place new order
-const newOrder = asyncHandler(
+const placeNewOrder = asyncHandler(
   async (
     req: Request<{}, {}, OrderItemRequest>,
     res: Response,
@@ -58,4 +62,119 @@ const newOrder = asyncHandler(
   }
 );
 
-export { newOrder };
+//* Get all orders - Admin
+const getAllOrders = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let orders;
+    if (await redisClient.exists("all-orders")) {
+      orders = await redisClient.get("all-orders");
+    } else {
+      orders = await Order.find();
+      await redisClient.set("all-orders", JSON.stringify(orders));
+    }
+
+    res.json(new responseHandler(200, "All orders", { orders }));
+  }
+);
+
+//* My Orders - User
+const myOrders = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id: userId } = req.query;
+    let orders;
+    if (await redisClient.exists(`my-orders:${userId}`)) {
+      orders = await redisClient.get(`my-orders:${userId}`);
+    } else {
+      orders = await Order.find({ userId });
+      await redisClient.set(`my-orders:${userId}`, JSON.stringify(orders));
+    }
+    res.json(new responseHandler(200, "My Orders", { orders }));
+  }
+);
+
+//* Get single order - Admin
+
+const getSingleOrder = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    let order;
+    if (await redisClient.exists(`order:${id}`)) {
+      order = await redisClient.get(`order:${id}`);
+    } else {
+      order = await Order.findById(id);
+      await redisClient.set(`order:${id}`, JSON.stringify(order));
+    }
+    if (!order) {
+      return next(new ErrorHandler("Order not found", 404));
+    }
+    res.json(new responseHandler(200, "Order", { order }));
+  }
+);
+
+//* Process Order - Admin
+
+const processOrder = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+    if (!order) {
+      return next(new ErrorHandler("Order not found", 404));
+    }
+
+    switch (order.status) {
+      case "Processing":
+        order.status = "Shipped";
+        break;
+      case "Shipped":
+        order.status = "Delivered";
+        break;
+      default:
+        order.status = "Already Delivered";
+        break;
+    }
+
+    await order.save();
+    res
+      .status(200)
+      .json(new responseHandler(200, "Order processed successfully", order));
+  }
+);
+
+//* Cancel Order - User
+const cancelOrder = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+    if (!order) {
+      return next(new ErrorHandler("Order not found", 404));
+    }
+
+    if (order.status !== "Processing") {
+      return next(new ErrorHandler("You cannot cancel this order", 400));
+    }
+
+    InvalidateCache({
+      order: true,
+      product: true,
+      admin: true,
+      userId: String(order.userId),
+      orderId: String(order._id),
+      productId: order.orderItems.map((item) => String(item.productId)),
+    });
+
+    await restoreStock(order.orderItems);
+    await order.deleteOne();
+
+    res
+      .status(200)
+      .json(new responseHandler(200, "Order canceled successfully", {}));
+  }
+);
+export {
+  placeNewOrder,
+  getAllOrders,
+  myOrders,
+  getSingleOrder,
+  processOrder,
+  cancelOrder,
+};

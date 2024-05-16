3e5ebd042238fff41b8a59a3fb2d257b6f6cdbc3
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-05-16 19:12:29 +0530

    feat: add content in main section of landing page

--- client/src/pages/user/landing/landing.tsx
+++ client/src/pages/user/landing/landing.tsx
@@ -9,6 +9,17 @@ const Landing = () => {
     <div className="landing">
       <main>
         <Header />
+
+        <div className="content">
+          <div className="info">
+            <h1>Make Your Interior More Minimalistic & Modern</h1>
+            <p>
+              Turn your room with panto into a lot more minimalist and modern
+              with ease and speed
+            </p>
+          </div>
+          <div className="sofa"></div>
+        </div>
       </main>
     </div>
   );

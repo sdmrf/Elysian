59dbce390ad0e481769cdd527802ce0d4e1964b2
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-05-16 22:05:28 +0530

    feat: add loader

--- client/src/components/common/loader/loader.tsx
+++ client/src/components/common/loader/loader.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+// Imports
+import "./_styles.scss";
 
 const Loader = () => {
   return (
-    <div>
-      
+    <div className="loader">
+      <div id="square1"></div>
+      <div id="square2"></div>
+      <div id="square3"></div>
+      <div id="square4"></div>
+      <div id="square5"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;

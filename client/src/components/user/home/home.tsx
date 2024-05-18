2390b475f0fc14e3333e53594f35ca6c65b44c6e
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-05-18 22:38:27 +0530

    feat: add card

--- client/src/components/user/home/home.tsx
+++ client/src/components/user/home/home.tsx
@@ -3,43 +3,13 @@ import SwiperSlider from "../../common/wrappers/swiper-wrapper/swiper-slider/swi
 import Card from "../../common/card/card";
 
 // Demo Data
-
-const furnitures = [
-  {
-    name: "Sakarias Armchair",
-    price: "$392",
-    image: "url_to_sakarias_armchair_image",
-    rating: 5,
-  },
-  {
-    name: "Baltsar Chair",
-    price: "$299",
-    image: "url_to_baltsar_chair_image",
-    rating: 4,
-  },
-  {
-    name: "Anjay Chair",
-    price: "$519",
-    image: "url_to_anjay_chair_image",
-    rating: 5,
-  },
-  {
-    name: "Nyantuy Chair",
-    price: "$921",
-    image: "url_to_nyantuy_chair_image",
-    rating: 4,
-  },
-];
+import { furnitures } from "../../../assets/data/homeData";
 
 const HomeX = () => {
   return (
     <div className="Home">
       <main>
-        <SwiperSlider>
-          {furnitures.map((furniture, index) => (
-            <Card key={index} {...furniture} />
-          ))}
-        </SwiperSlider>
+        <Card {...furnitures[0]} />
       </main>
     </div>
   );

7e2c45ff8977c2115d13fb0b3b1067ef8aa917b9
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-05-19 23:26:33 +0530

    feat: initialise logo-slider

--- client/src/components/common/wrappers/swiper-wrapper/swiper-carousel/swiperCarousel.tsx
+++ client/src/components/common/wrappers/swiper-wrapper/swiper-carousel/swiperCarousel.tsx
@@ -20,48 +20,50 @@ import "swiper/css/effect-coverflow";
 
 const SwiperCarousel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Swiper
-      modules={[
-        Navigation,
-        Pagination,
-        Scrollbar,
-        A11y,
-        Autoplay,
-        EffectCoverflow,
-      ]}
-      effect={"coverflow"}
-      grabCursor={true}
-      centeredSlides={true}
-      autoplay={{ delay: 5000 }}
-      loop={true}
-      slidesPerView={"auto"}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 25,
-        depth: 150,
-        modifier: 2.5,
-        slideShadows: true,
-      }}
-      pagination={{ el: ".swiperPagination", clickable: true }}
-      navigation={{
-        nextEl: ".swiperBtnNext",
-        prevEl: ".swiperBtnPrev",
-      }}
-      className="swiperCarousel"
-    >
-      {React.Children.map(children, (child, index) => (
-        <SwiperSlide key={index}>{child}</SwiperSlide>
-      ))}
-      <div className="sliderController">
-        <div className="swiperBtnPrev">
-          <CaretLeft className="icon" weight="bold" />
+    <div className="carouselContainer">
+      <Swiper
+        modules={[
+          Navigation,
+          Pagination,
+          Scrollbar,
+          A11y,
+          Autoplay,
+          EffectCoverflow,
+        ]}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        autoplay={{ delay: 5000 }}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 25,
+          depth: 150,
+          modifier: 2.5,
+          slideShadows: true,
+        }}
+        pagination={{ el: ".swiperPagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiperBtnNext",
+          prevEl: ".swiperBtnPrev",
+        }}
+        className="swiperCarousel"
+      >
+        {React.Children.map(children, (child, index) => (
+          <SwiperSlide key={index}>{child}</SwiperSlide>
+        ))}
+        <div className="sliderController">
+          <div className="swiperBtnPrev">
+            <CaretLeft className="icon" weight="bold" />
+          </div>
+          <div className="swiperBtnNext">
+            <CaretRight className="icon" weight="bold" />
+          </div>
+          <div className="swiperPagination"></div>
         </div>
-        <div className="swiperBtnNext">
-          <CaretRight className="icon" weight="bold" />
-        </div>
-        <div className="swiperPagination"></div>
-      </div>
-    </Swiper>
+      </Swiper>
+    </div>
   );
 };
 
---
+++ client/src/components/common/wrappers/swiper-wrapper/swiper-logo/swiperLogo.tsx
@@ -0,0 +1,30 @@
+// Imports
+import React from "react";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+const SwiperLogo = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="logoContainer">
+      <Swiper
+        modules={[Autoplay]}
+        loop={true}
+        slidesPerView={"auto"}
+        speed={5000}
+        allowTouchMove={false}
+        autoplay={{
+          delay: 1,
+          disableOnInteraction: false,
+        }}
+        className="swiperLogo"
+      >
+        {React.Children.map(children, (child, index) => (
+          <SwiperSlide key={index}>{child}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default SwiperLogo;
